73db40fb07d9e110d9ffafcf16b3d79a10bccd7a
Mark Fisher <logicfish@gmail.com>
2020-03-16 18:36:14 +0000

    Interp changed to yeild Variant[] rather than string

--- source/metad/interp.d
+++ source/metad/interp.d
@@ -2,46 +2,48 @@ module metad.interp;
 
 private import pegged.grammar;
 
-private import std.array;
+private import std.variant;
 private import std.typecons;
 private import std.algorithm;
 
 private import metad.compiler;
 
 /++
- + The run-time verstion of the meta-compiler (meta-compiler).
- +/
++ The run-time verstion of the meta-compiler (meta-compiler).
++/
 
- template loadGrammarAndData(string grammarFile,string docRoot) {
+template loadGrammarAndData(string grammarFile,string docRoot) {
     mixin loadGrammar!(grammmarFile,docRoot);
     auto loadGrammarAndData(string dataString) {
         return parser(dataString);
     }
- }
+}
 
- template loadAndInterpret(string grammarFile,string docRoot,alias _Interpreter) {
-     auto loadAndInterpret(string data) {
-       auto _data = loadGrammarAndData(grammarFile,docRoot)(data);
-       return Interpreter!(_Interpreter)(_data);
-     }
- }
+template loadAndInterpret(string grammarFile,string docRoot,alias _Interpreter) {
+	auto loadAndInterpret(string data) {
+		auto _data = loadGrammarAndData(grammarFile,docRoot)(data);
+		return Interpreter!(_Interpreter)(_data);
+	}
+}
 
-template Interpreter(T,N) {
-  static T[] interpChildNodes(N nodes,ParseTree t) {
-      T[] result;
-      foreach(x;t.children) {
-          result~=Interpreter!(T,N)(nodes,x);
-      }
-      return result;
-  }
+template Interpreter(N) {
+	static Variant[] interpChildNodes(N nodes,ParseTree t) {
+		Variant[] result;
+		foreach(x;t.children) {
+			result~=Interpreter!N(nodes,x);
+		}
+		return result;
+	}
     //return Parser(data.name)(data);
-  T[] Interpreter(N nodes,ParseTree data) {
-    import std.stdio;
-    if(data.name in nodes) {
-      return [nodes[data.name](data)];
-    }
-    return interpChildNodes(nodes,data);
-  }
+	Variant[] Interpreter(N nodes,ParseTree data) {
+		import std.stdio;
+		writeln("Interpreter " ~ data.name);
+		if(data.name in nodes) {
+			return [nodes[data.name](data)];
+		}
+		//return interpChildNodes(nodes,data).join("");
+		return interpChildNodes(nodes,data);
+	}
 }
 
 unittest {
@@ -56,14 +58,14 @@ unittest {
     // A grammar that parses {{Template}}
     // statements in a body of text.
     enum _g = q{
-GRAMMAR(Template):
-    Doc <- Line+ :endOfInput
-    Line <- (Var / Text)
-    Var <- :LDelim ^Template :RDelim
-    LDelim <- "{{"
-    RDelim <- "}}"
-    Text <- ~((!LDelim) Char )*
-    Char <- .
+		GRAMMAR(Template):
+		Doc <- Line+ :endOfInput
+			Line <- (Var / Text)
+			Var <- :LDelim ^Template :RDelim
+			LDelim <- "{{"
+			RDelim <- "}}"
+			Text <- ~((!LDelim) Char )*
+			Char <- .
     };
     mixin(grammar(_g));
 
@@ -81,27 +83,31 @@ GRAMMAR(Template):
         };
     };
     static string idParser(string s) {
-      writeln("idParser" ~ s);
-      if(s=="__T") {
-        return __T;
-      } else if (s=="__M") {
-        return __M;
-      }
-      return s;
+		writeln("idParser" ~ s);
+		if(s=="__T") {
+			return __T;
+		} else if (s=="__M") {
+			return __M;
+		}
+		return s;
     }
     // Create a compiler from the parse tree.
     auto myInterpreter(ParseTree t) {
-      string delegate(ParseTree)[string] nodes;
-      //nodes["GRAMMAR.Var"] = f=>idParser(f.children[1].matches);
-      nodes["GRAMMAR.LDelim"] = f=>"";
-      nodes["GRAMMAR.RDelim"] = f=>"";
-      nodes["GRAMMAR.Text"] = f=>f.matches.join("");
-      nodes["identifier"] = (f)=>idParser(f.matches.join(""));
+		Variant delegate(ParseTree)[string] nodes;
+		//nodes["GRAMMAR.Var"] = f=>idParser(f.children[1].matches);
+		nodes["GRAMMAR.LDelim"] = f=>Variant();
+		nodes["GRAMMAR.RDelim"] = f=>Variant();
+		nodes["GRAMMAR.Text"] = f=>Variant(f.matches.join(""));
+		nodes["identifier"] = (f)=>Variant(idParser(f.matches.join("")));
 
-      return Interpreter!(string,typeof(nodes))(nodes,t).join("");
+		return Interpreter(nodes,t);
     }
     auto interp = myInterpreter(GRAMMAR!identifier(_d));
-    writeln(" INTERP: " ~ interp);
-    auto expected = _d.replace("{{__T}}",__T).replace("{{__M}}",__M);
-    assert(interp == expected,"Expected: "~expected ~ "\nGot: "~interp);
+	
+	import std.conv;
+	auto interpString = interp.map!(x=>x.to!string).join("");
+    writeln(" INTERP: " ~ interpString );
+    
+	auto expected = _d.replace("{{__T}}",__T).replace("{{__M}}",__M);
+    assert(interpString == expected,"Expected: "~expected ~ "\nGot: "~interpString);
 }
